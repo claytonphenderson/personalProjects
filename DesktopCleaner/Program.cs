21a001a57ef08518aefec96a4c70469de50878b2
Clayton Henderson <claytonphenderson@gmail.com>
2017-02-25 13:42:47 -0600

    Add files via upload
    just a simple desktop cleaner for mom and dads mac. to be run once daily.  written in dotnet core

---
+++ DesktopCleaner/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+namespace ConsoleApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] files = System.IO.Directory.GetFiles("/Users/claytonhenderson/Desktop/");
+
+			//create target file in documents
+			string targetDirectory = "/Users/claytonhenderson/Documents/DesktopCopy" + DateTime.Now.Month + "-" + DateTime.Now.Day + "-" + DateTime.Now.Year;
+			Directory.CreateDirectory(targetDirectory);
+
+			foreach (string s in files)
+			{
+				string fileName = Path.GetFileName(s);
+				string documentsDestination = Path.Combine(targetDirectory, fileName);
+				File.Copy(s, documentsDestination, true);
+				//Console.WriteLine("Moved " + fileName + " to documents folder.");
+				File.Delete(s);
+			}
+			//Console.ReadKey();
+		}
+	}
+}
